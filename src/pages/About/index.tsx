c6444ce999a79a477579f682c9504276e27452a7
Damilare Tolulope <damilaretolulope1@gmail.com>
2024-10-28 08:20:06 +0100

    chore: about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,9 +1,154 @@
+import Button from "@/components/Button";
+import { teams, values } from "@/constants/data";
 
+import circle from "@/assets/icons/circle.svg";
+import about1 from "@/assets/images/about (1).png";
+import about2 from "@/assets/images/about (2).png";
+import about3 from "@/assets/images/about (3).png";
+import about4 from "@/assets/images/about (4).png";
+import about5 from "@/assets/images/about (5).png";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="px-5 md:px-10 lg:px-20 bg-primary py-40 text-white">
+        <div className="text-center w-3/4 mx-auto">
+          <p className="text-center text-[#F1F1F1] text-xl mb-4">About</p>
+          <p className="text-6xl leading-[72px] lg:w-4/5 mx-auto text-center">
+            We love to make great things, things that matter.
+          </p>
+          <p className="lg:w-3/4 mx-auto font-normal text-[#F1F1F1] pt-6 mb-10">
+            Funding handshake buyer business-to-business metrics iPad
+            partnership. First mover advantage innovator success deployment
+            non-disclosure.
+          </p>
+        </div>
+      </div>
 
-export default About
+      <div className="relative px-5 md:px-10 lg:px-20 py-20">
+        <div className="opacity-0 grid grid-cols-1 md:grid-cols-3 gap-10 lg:px-10">
+          <img src={about1} className="w-full h-full" alt="im" />
+          <img src={about2} className="w-full row-span-2 h-full" alt="im" />
+          <img src={about3} className="w-full h-full" alt="im" />
+          <img src={about4} className="w-full h-full" alt="im" />
+          <img src={about5} className="w-full h-full" alt="im" />
+        </div>
+        <div className="absolute -top-28 grid grid-cols-1 md:grid-cols-3 gap-10 mb-20 lg:px-10">
+          <img src={about1} className="w-full h-full" alt="im" />
+          <img src={about2} className="w-full row-span-2 h-full" alt="im" />
+          <img src={about3} className="w-full h-full" alt="im" />
+          <img src={about4} className="w-full h-full" alt="im" />
+          <img src={about5} className="w-full h-full" alt="im" />
+        </div>
+        <div className="lg:w-3/5 mx-auto">
+        <p className="text-[#777777] text-xl mb-4">Our story</p>
+        <p className="text-5xl leading-[72px] mx-auto">
+          Handshake infographic mass market crowdfunding iteration.
+        </p>
+        <div className="mt-6 text-[#777777] text-xl">
+          Conversion angel investor entrepreneur first mover advantage.
+          Handshake infographic mass market crowdfunding iteration. Traction
+          stock user experience deployment beta innovator incubator focus. Sales
+          user experience branding growth hacking holy grail monetization
+          conversion prototype stock network effects. Learning curve network
+          effects return on investment bootstrapping business-to-consumer.{" "}
+        </div>
+        </div>
+      </div>
+
+      <div className="px-5 md:px-10 lg:px-20 bg-primary py-20 text-white">
+        <p className="text-center text-[#F1F1F1] text-xl mb-4">Our numbers</p>
+        <p className="text-5xl leading-[72px] lg:w-4/5 mx-auto text-center">
+          Handshake infographic mass market crowdfunding iteration.
+        </p>
+        <div className="flex items-center justify-between px-10">
+          <div>
+            <p className="text-[96px] text-[#4FE9A4]">120m</p>
+            <p className="text-2xl text-[#F1F1F1]">Cool feature title</p>
+          </div>
+          <div>
+            <p className="text-[96px] text-[#4FE9A4]">10.000</p>
+            <p className="text-2xl text-[#F1F1F1]">Cool feature title</p>
+          </div>
+          <div>
+            <p className="text-[96px] text-[#4FE9A4]">240</p>
+            <p className="text-2xl text-[#F1F1F1]">Cool feature title</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-5 md:px-10 lg:px-20 py-20">
+        <div className="lg:w-3/5 mx-auto">
+          <p className="text-[#777777] text-xl mb-4">Our team</p>
+          <p className="text-5xl leading-[72px] mx-auto">The leadership team</p>
+          <p className="text-[#777777] text-lg mt-5">
+            Conversion angel investor entrepreneur first mover advantage.
+            Handshake infographic mass market crowdfunding iteration. Traction
+            stock user experience deployment beta innovator incubator focus.{" "}
+          </p>
+        </div>
+
+        <div className="lg:w-4/5 mx-auto mt-16 grid grid-cols-1 md:grid-cols-3 gap-10">
+          {teams.map(({ img, name, role }) => (
+            <div key={name}>
+              <img src={img} alt={name} />
+              <p className="text-3xl mb-3 mt-6">{name}</p>
+              <p className="text-xl text-[#777777]">{role}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="bg-primary px-5 md:px-10 lg:px-20 py-20 text-white">
+        <div className="lg:w-3/5 mx-auto">
+          <p className="text-[#777777] text-xl mb-4">Our values</p>
+          <p className="text-5xl leading-[72px] mx-auto">
+            Things in we believe
+          </p>
+          <p className="text-[#F1F1F1] text-lg mt-5">
+            Conversion angel investor entrepreneur first mover advantage.
+            Handshake infographic mass market crowdfunding iteration. Traction
+            stock user experience deployment beta innovator incubator focus.{" "}
+          </p>
+
+          <div className="mt-16">
+            {values.map(({ title, desc, img }) => (
+              <div className="flex items-start gap-12 mb-16" key={title}>
+                <img src={img} alt={title} />
+                <div>
+                  <p className="text-3xl">{title}</p>
+                  <p className="text-xl mt-4">{desc}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="px-5 md:px-10 lg:px-20 py-20">
+        <div className="relative bg-primary text-white py-20 rounded-2xl">
+          <img
+            src={circle}
+            alt="circle"
+            className="absolute top-0 right-0 w-2/5 h-4/5 rounded-2xl opacity-30"
+          />
+          <p className="text-5xl leading-[72px] lg:w-3/5 mx-auto text-center">
+            An enterprise template to ramp up your company website
+          </p>
+          <div className="flex items-center justify-center gap-5 mt-10">
+            <input
+              type="email"
+              className="border-2 border-white bg-white text-xl text-black placeholder:text-black rounded-full px-10 py-4 w-1/3"
+              placeholder="Your email address"
+            />
+            <Button className="bg-[#65E4A3] border-[#65E4A3] text-primary">
+              Start Now
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,28 @@
-import Button from "@/components/Button"
-import { useEffect } from "react"
-import { blogs, faq, services, testimonials } from "@/constants/data"
-import { ArrowLeft, ArrowRight, ChevronDownIcon, Dot, Eye, Feather, Sun } from "lucide-react"
+import Button from "@/components/Button";
+import { useEffect } from "react";
+import { blogs, faq, services, testimonials } from "@/constants/data";
+import {
+  ArrowLeft,
+  ArrowRight,
+  ChevronDownIcon,
+  Dot,
+  Eye,
+  Feather,
+  Sun,
+} from "lucide-react";
 
-import user from "@/assets/images/Rectangle 1270.png"
-import user1 from "@/assets/images/Rectangle 1270-1.png"
-import check from "@/assets/icons/check.svg"
-import graph from "@/assets/icons/graph.svg"
-import pie from "@/assets/icons/pie.svg"
-import boldo from "@/assets/icons/boldo.svg"
-import presto from "@/assets/icons/presto.svg"
-import info1 from "@/assets/icons/mini.svg"
-import info2 from "@/assets/images/Frame 219.png"
-import info3 from "@/assets/icons/mini1.svg"
-import circle from "@/assets/icons/circle.svg"
-import image from "@/assets/images/Image.png"
+import user from "@/assets/images/Rectangle 1270.png";
+import user1 from "@/assets/images/Rectangle 1270-1.png";
+import check from "@/assets/icons/check.svg";
+import graph from "@/assets/icons/graph.svg";
+import pie from "@/assets/icons/pie.svg";
+import boldo from "@/assets/icons/boldo.svg";
+import presto from "@/assets/icons/presto.svg";
+import info1 from "@/assets/icons/mini.svg";
+import info2 from "@/assets/images/Frame 219.png";
+import info3 from "@/assets/icons/mini1.svg";
+import circle from "@/assets/icons/circle.svg";
+import image from "@/assets/images/Image.png";
 
 const Home = () => {
   const logosContainer = document.getElementById("logos");
@@ -23,36 +31,53 @@ const Home = () => {
   useEffect(() => {
     window.addEventListener("load", () => {
       self.setInterval(() => {
-        if(logosContainer?.scrollLeft !== logosContainerScrollWidth) {
-          logosContainer?.scrollTo(logosContainer.scrollLeft + 1, 0)
+        if (logosContainer?.scrollLeft !== logosContainerScrollWidth) {
+          logosContainer?.scrollTo(logosContainer.scrollLeft + 1, 0);
         }
-      }, 15)
-    })
-  }, [])
+      }, 15);
+    });
+  }, []);
 
   return (
     <div>
       <div className="relative px-5 md:px-10 lg:px-20 pt-10 bg-primary h-screen">
-        <img src={circle} alt="circle" className="absolute top-0 right-0 w-1/2 opacity-30" />
+        <img
+          src={circle}
+          alt="circle"
+          className="absolute top-0 right-0 w-1/2 opacity-30"
+        />
         <div className="grid grid-cols-1 md:grid-cols-2 pt-60 items-center">
           <div>
-            <h1 className="text-white font-normal text-5xl leading-[72px]">Save time by building  fast with Boldo Template</h1>
-            <p className="font-normal text-[#F1F1F1] pt-4 mb-10">Funding handshake buyer business-to-business metrics iPad partnership. First mover advantage innovator success deployment non-disclosure.</p>
+            <h1 className="text-white font-normal text-5xl leading-[72px]">
+              Save time by building fast with Boldo Template
+            </h1>
+            <p className="font-normal text-[#F1F1F1] pt-4 mb-10">
+              Funding handshake buyer business-to-business metrics iPad
+              partnership. First mover advantage innovator success deployment
+              non-disclosure.
+            </p>
             <div className="flex items-center gap-6">
-              <Button className="bg-[#65E4A3] border-[#65E4A3] text-primary">Buy Template</Button>
-              <Button className="" isSecondary>Explore</Button>
+              <Button className="bg-[#65E4A3] border-[#65E4A3] text-primary">
+                Buy Template
+              </Button>
+              <Button className="" isSecondary>
+                Explore
+              </Button>
             </div>
           </div>
           <div className="mx-auto">
             <img src={info2} alt="info2" />
             <div className="flex items-center mt-5 gap-5">
-            <img src={info1} alt="info1" className="w-full h-full" />
-            <img src={info3} alt="info3" className="w-full h-full" />
+              <img src={info1} alt="info1" className="w-full h-full" />
+              <img src={info3} alt="info3" className="w-full h-full" />
             </div>
           </div>
         </div>
 
-        <div className="flex items-center justify-between mt-20 whitespace-nowrap overflow-x-scroll w-full py-10 relative" id="logos">
+        <div
+          className="flex items-center justify-between mt-20 whitespace-nowrap overflow-x-scroll w-full py-10 relative"
+          id="logos"
+        >
           <div className="absolute h-full bg-gradient-to-l to-primary from-transparent px-20"></div>
           <img src={boldo} alt="boldo" />
           <img src={presto} alt="presto" />
@@ -67,21 +92,23 @@ const Home = () => {
       {/* Services */}
       <div className="px-5 md:px-10 lg:px-20 py-20">
         <p className="text-center text-[#777777] text-xl mb-4">Our Services</p>
-        <p className="text-5xl leading-[72px] lg:w-4/5 mx-auto text-center">Handshake infographic mass market crowdfunding iteration.</p>
+        <p className="text-5xl leading-[72px] lg:w-4/5 mx-auto text-center">
+          Handshake infographic mass market crowdfunding iteration.
+        </p>
 
         <div className="my-20 flex flex-col md:flex-row items-stretch justify-between gap-20">
-          {
-            services.map(({title, desc, img}, index) => (
-              <div key={index} className="w-full lg:w-1/3">
-                <img src={img} alt={title} className="w-full" />
-                <div className="pt-6">
-                  <p className="mb-3 text-2xl">{title}</p>
-                  <p className="mb-7 text-[#777777] text-xl">{desc}</p>
-                </div>
-                <button className="text-primary border-b border-primary flex items-center gap-3 py-2">Explore Page <ArrowRight /></button>
+          {services.map(({ title, desc, img }, index) => (
+            <div key={index} className="w-full lg:w-1/3">
+              <img src={img} alt={title} className="w-full" />
+              <div className="pt-6">
+                <p className="mb-3 text-2xl">{title}</p>
+                <p className="mb-7 text-[#777777] text-xl">{desc}</p>
               </div>
-            ))
-          }
+              <button className="text-primary border-b border-primary flex items-center gap-3 py-2">
+                Explore Page <ArrowRight />
+              </button>
+            </div>
+          ))}
         </div>
 
         <div className="my-20 grid grid-cols-1 md:grid-cols-2 items-center">
@@ -96,11 +123,22 @@ const Home = () => {
             </div>
           </div>
           <div className="w-3/4 mx-auto">
-            <p className="text-4xl leading-[56px]">We connect our customers with the best, and help them keep up-and stay open.</p>
+            <p className="text-4xl leading-[56px]">
+              We connect our customers with the best, and help them keep up-and
+              stay open.
+            </p>
             <ul className="mt-10 mb-14">
-              <li className="flex items-center gap-3 mb-6"><img src={check} alt="check" /> We connect our customers with the best.</li>
-              <li className="flex items-center gap-3 mb-6"><img src={check} alt="check" /> Advisor success customer launch party.</li>
-              <li className="flex items-center gap-3"><img src={check} alt="check" /> Business-to-consumer long tail.</li>
+              <li className="flex items-center gap-3 mb-6">
+                <img src={check} alt="check" /> We connect our customers with
+                the best.
+              </li>
+              <li className="flex items-center gap-3 mb-6">
+                <img src={check} alt="check" /> Advisor success customer launch
+                party.
+              </li>
+              <li className="flex items-center gap-3">
+                <img src={check} alt="check" /> Business-to-consumer long tail.
+              </li>
             </ul>
             <Button className="!bg-primary border-primary">Start Now</Button>
           </div>
@@ -108,11 +146,20 @@ const Home = () => {
 
         <div className="my-20 mt-40 grid grid-cols-1 md:grid-cols-2 items-center">
           <div className="w-3/4 mx-auto">
-            <p className="text-4xl leading-[56px]">We connect our customers with the best, and help them keep up-and stay open.</p>
+            <p className="text-4xl leading-[56px]">
+              We connect our customers with the best, and help them keep up-and
+              stay open.
+            </p>
             <ul className="mt-10 mb-14">
-              <li className="shadow-sm shadow-black/5 rounded-sm bg-primary text-white p-4 px-5 flex items-center gap-3 mb-6"><Feather /> We connect our customers with the best.</li>
-              <li className="shadow-sm shadow-black/5 rounded-sm bg-white p-4 px-5 flex items-center gap-3 mb-6"><Eye /> Advisor success customer launch party.</li>
-              <li className="shadow-sm shadow-black/5 rounded-sm bg-white p-4 px-5 flex items-center gap-3"><Sun /> Business-to-consumer long tail.</li>
+              <li className="shadow-sm shadow-black/5 rounded-sm bg-primary text-white p-4 px-5 flex items-center gap-3 mb-6">
+                <Feather /> We connect our customers with the best.
+              </li>
+              <li className="shadow-sm shadow-black/5 rounded-sm bg-white p-4 px-5 flex items-center gap-3 mb-6">
+                <Eye /> Advisor success customer launch party.
+              </li>
+              <li className="shadow-sm shadow-black/5 rounded-sm bg-white p-4 px-5 flex items-center gap-3">
+                <Sun /> Business-to-consumer long tail.
+              </li>
             </ul>
           </div>
           <div className="relative">
@@ -120,9 +167,17 @@ const Home = () => {
             <div className="absolute left-20 -bottom-28 bg-white shadow-sm p-10 rounded-xl">
               <img src={pie} alt="pie" />
               <div className="mt-5">
-                <p className="text-xs mb-3 flex items-center"><Dot className="mr-1 text-[#0DBBFC]" /> 35% -  Agile Development</p>
-                <p className="text-xs mb-3 flex items-center"><Dot className="mr-1 text-[#0DBBFC]" /> 30% -  Investor bandwidth</p>
-                <p className="text-xs flex items-center"><Dot className="mr-1 text-[#0DBBFC]" /> 35% -  A/B testing </p>
+                <p className="text-xs mb-3 flex items-center">
+                  <Dot className="mr-1 text-[#0DBBFC]" /> 35% - Agile
+                  Development
+                </p>
+                <p className="text-xs mb-3 flex items-center">
+                  <Dot className="mr-1 text-[#0DBBFC]" /> 30% - Investor
+                  bandwidth
+                </p>
+                <p className="text-xs flex items-center">
+                  <Dot className="mr-1 text-[#0DBBFC]" /> 35% - A/B testing{" "}
+                </p>
               </div>
             </div>
           </div>
@@ -132,96 +187,124 @@ const Home = () => {
       {/*  */}
       <div className="px-5 md:px-10 lg:px-20 py-20 bg-primary text-white">
         <div className="flex justify-between items-end">
-      <p className="text-5xl leading-[72px] lg:w-3/5">An enterprise template to ramp up your company website</p>
+          <p className="text-5xl leading-[72px] lg:w-3/5">
+            An enterprise template to ramp up your company website
+          </p>
           <div className="flex items-center gap-4">
-            <ArrowLeft className="bg-white p-3 cursor-pointer text-primary hover:scale-105 duration-300 transition-all rounded-full" size={40} />
-            <ArrowRight className="bg-white p-3 cursor-pointer text-primary hover:scale-105 duration-300 transition-all rounded-full" size={40} />
+            <ArrowLeft
+              className="bg-white p-3 cursor-pointer text-primary hover:scale-105 duration-300 transition-all rounded-full"
+              size={40}
+            />
+            <ArrowRight
+              className="bg-white p-3 cursor-pointer text-primary hover:scale-105 duration-300 transition-all rounded-full"
+              size={40}
+            />
           </div>
         </div>
 
         <div className="mt-16 flex items-start flex-col md:flex-row gap-6 md:px-10">
-          {
-            testimonials.map(({ testimony, img, name, role}) => (
-              <div key={name} className="w-full p-10 rounded-xl bg-white shadow-lg shadow-black/10">
-                <p className="text-2xl mb-10 text-black">“{testimony}”</p>
-                <div className="flex gap-3 items-center">
-                  <img src={img} alt={name} />
-                  <div className="text-primary">
-                    <p className="font-bold">{name}</p>
-                    <p className="text-sm mt-1">{role}</p>
-                  </div>
+          {testimonials.map(({ testimony, img, name, role }) => (
+            <div
+              key={name}
+              className="w-full p-10 rounded-xl bg-white shadow-lg shadow-black/10"
+            >
+              <p className="text-2xl mb-10 text-black">“{testimony}”</p>
+              <div className="flex gap-3 items-center">
+                <img src={img} alt={name} />
+                <div className="text-primary">
+                  <p className="font-bold">{name}</p>
+                  <p className="text-sm mt-1">{role}</p>
                 </div>
               </div>
-            ))
-          }
+            </div>
+          ))}
         </div>
       </div>
 
-    <div className="px-5 md:px-10 lg:px-20 py-20 mt-10">
-<img src={image} alt="s-image" className="rounded-3xl w-full mb-14" />
-<div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 items-center">
-  <p className="text-4xl leading-[56px]">
-  We connect our customers with the best, and help them keep up-and stay open.
-  </p>
+      <div className="px-5 md:px-10 lg:px-20 py-20 mt-10">
+        <img src={image} alt="s-image" className="rounded-3xl w-full mb-14" />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 items-center">
+          <p className="text-4xl leading-[56px]">
+            We connect our customers with the best, and help them keep up-and
+            stay open.
+          </p>
           <div className="">
-            {
-              faq.map(({ question }) => (
-                <div key={question} className="w-5/6 mx-auto">
-                  <p className="border-b border-[#C4C4C4] py-4 flex items-center justify-between cursor-pointer">
-                    {question} <ChevronDownIcon className="bg-primary p-1 rounded-full text-white" size={35} />
-                  </p>
-                </div>
-              ))
-            }
+            {faq.map(({ question }) => (
+              <div key={question} className="w-5/6 mx-auto">
+                <p className="border-b border-[#C4C4C4] py-4 flex items-center justify-between cursor-pointer">
+                  {question}{" "}
+                  <ChevronDownIcon
+                    className="bg-primary p-1 rounded-full text-white"
+                    size={35}
+                  />
+                </p>
+              </div>
+            ))}
           </div>
-</div>
-          </div>
+        </div>
+      </div>
 
       {/* Blog */}
       <div className="px-5 md:px-10 lg:px-20 py-20">
         <p className="text-center text-[#777777] text-xl mb-4">Our Blog</p>
-        <p className="text-5xl leading-[72px] lg:w-4/5 mx-auto text-center">Value proposition accelerator product management venture</p>
+        <p className="text-5xl leading-[72px] lg:w-4/5 mx-auto text-center">
+          Value proposition accelerator product management venture
+        </p>
 
         <div className="mt-20 flex flex-col md:flex-col items-stretch gap-10">
-          {
-            blogs.map(({ title, author, authorImg, category, date, img }) => (
-              <div key={title} className="w-full">
-                <div className="flex flex-col gap-20 justify-between">
-                  <div>
-                <img src={img} alt={title} className="w-full" />
-                <div className="mt-6">
-                  <div className="flex items-center gap-2 text-primary font-bold mb-3">{category} <span className="text-[#777777] font-normal">{date}</span></div>
-                  <p className="text-xl">{title}</p>
+          {blogs.map(({ title, author, authorImg, category, date, img }) => (
+            <div key={title} className="w-full">
+              <div className="flex flex-col gap-20 justify-between">
+                <div>
+                  <img src={img} alt={title} className="w-full" />
+                  <div className="mt-6">
+                    <div className="flex items-center gap-2 text-primary font-bold mb-3">
+                      {category}{" "}
+                      <span className="text-[#777777] font-normal">{date}</span>
+                    </div>
+                    <p className="text-xl">{title}</p>
                   </div>
                 </div>
-                  <div className="flex items-center gap-3">
-                <img src={authorImg} alt={author} className="" />
-                    <p>{author}</p>
-                  </div>
+                <div className="flex items-center gap-3">
+                  <img src={authorImg} alt={author} className="" />
+                  <p>{author}</p>
                 </div>
               </div>
-            ))
-          }
+            </div>
+          ))}
         </div>
 
         <div className="flex items-center justify-center">
-          <Button className="mt-20 border-primary text-primary" isSecondary>Load More</Button>
-        </div>
-    </div>
-
-    <div className="px-5 md:px-10 lg:px-20 py-20">
-      <div className="relative bg-primary text-white py-20 rounded-2xl">
-      <img src={circle} alt="circle" className="absolute top-0 right-0 w-2/5 h-4/5 rounded-2xl opacity-30" />
-        <p className="text-5xl leading-[72px] lg:w-3/5 mx-auto text-center">An enterprise template to ramp up your company website</p>
-        <div className="flex items-center justify-center gap-5 mt-10">
-          <input type="email" className="border-2 border-white bg-white text-xl text-black placeholder:text-black rounded-full px-10 py-4 w-1/3" placeholder="Your email address" />
-        <Button className="bg-[#65E4A3] border-[#65E4A3] text-primary">Start Now</Button>
+          <Button className="mt-20 border-primary text-primary" isSecondary>
+            Load More
+          </Button>
         </div>
       </div>
 
+      <div className="px-5 md:px-10 lg:px-20 py-20">
+        <div className="relative bg-primary text-white py-20 rounded-2xl">
+          <img
+            src={circle}
+            alt="circle"
+            className="absolute top-0 right-0 w-2/5 h-4/5 rounded-2xl opacity-30"
+          />
+          <p className="text-5xl leading-[72px] lg:w-3/5 mx-auto text-center">
+            An enterprise template to ramp up your company website
+          </p>
+          <div className="flex items-center justify-center gap-5 mt-10">
+            <input
+              type="email"
+              className="border-2 border-white bg-white text-xl text-black placeholder:text-black rounded-full px-10 py-4 w-1/3"
+              placeholder="Your email address"
+            />
+            <Button className="bg-[#65E4A3] border-[#65E4A3] text-primary">
+              Start Now
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
